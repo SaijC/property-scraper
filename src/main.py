2d232c0c6d414de4d166fe5a12e6d98cb72cd812
fchen <fm_c@hotmail.com>
2026-01-12 11:01:50 +0000

    test setup

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
-from src.scraper.scraper_base import fetch_page, parse_headlines
-from src.utils.utils import save_to_csv
+from scraper.scraper_base import Scraper
+from utils.utils import save_to_csv
 
 def main():
-    url = "https://example-news-site.com"  # Replace with real site
-    html = fetch_page(url)
-    
+    scraper = Scraper()
+
+    url = "https://stackoverflow.com/questions/46775952/how-to-move-a-line-up-or-down-in-visual-studio-code-for-mac"
+    html = scraper.fetch_page(url)
+
     if html:
-        headlines = parse_headlines(html)
+        headlines = scraper.parse_headlines(html)
         if headlines:
             save_to_csv(headlines, "data/headlines.csv")
         else:
